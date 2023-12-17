ee48ab8129805517fc8c2e0473614a4a00960351
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-12-17 03:59:17 +0300

    db connection set up

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker
+ 
+engine = create_engine("postgresql://nelly:1YpddsZtoXczvA2pr0Q5K9JqOkMb62ml@dpg-clv4c9la73kc73bmg7i0-a.singapore-postgres.render.com/movies_fial")
+
+LocalSession = sessionmaker(bind=engine)
+
+def get_db():
+    db=LocalSession()
+    try:
+        yield db
+    finally:
+        db.close()
