199d543d3739680c3e5e6deb3112a9d208f6c5a7
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-12-17 03:17:56 +0300

    basic routers

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {"message": "welcome to movies-database"}
+
+@app.get('/movies/{movie_id}')
+def movie():
+    return{}
+
+@app.post('/movies')
+def create():
+    return{"message":"movie database created sucessfully"}
+
+@app.patch('/movies/{movie_id}')
+def update_movies(movie_id: int):
+    return {"message":f"Movie {movie_id} updated successfully"}
+
+@app.delete('/movies/{movie_id}')
+def delete_movies(movie_id: int):
+    return {"message":f"Movie {movie_id} deleted successfully"}
