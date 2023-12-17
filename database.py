5673abd24f4b19c1ba38ddc1145532e38c0fdd2d
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-12-17 05:29:14 +0300

    changes

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import sessionmaker
  
 engine = create_engine("postgresql://nelly:1YpddsZtoXczvA2pr0Q5K9JqOkMb62ml@dpg-clv4c9la73kc73bmg7i0-a.singapore-postgres.render.com/movies_fial")
 
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import session
+from database import get_db
 from schemas import MovieSchema
+from models import Movie
 
 app = FastAPI()
 
@@ -7,13 +10,23 @@ app = FastAPI()
 def index():
     return {"message": "welcome to movies-database"}
 
+@app.get('/movies')
+def movies(db:session=Depends(get_db)):
+    movies = db.query(Movie).all()
+    return movies
+
 @app.get('/movies/{movie_id}')
 def movie():
     return{}
 
 @app.post('/movies')
-def create_movie(movie: MovieSchema):
-    return{"message":"movie database created sucessfully"}
+def create_movie(movie: MovieSchema, db:session=Depends(get_db)):
+    print(movie)
+    new_movie = Movie(**movie.dict())
+    db.add(new_movie)
+    db.commit()
+    db.refresh(new_movie)
+    return{"mesage": "successfully created", "movie": new_movie}
 
 @app.patch('/movies/{movie_id}')
 def update_movies(movie_id: int):
